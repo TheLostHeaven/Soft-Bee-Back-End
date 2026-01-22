9a132f8d99dbcc9298199326a36e5454b129426c
TheLostHeaven <dannyking07@outlook.es>
2026-01-22 13:29:50 -0500

    feat (HU-000002): config apiaries imports

--- src/core/dependencies/containers.py
+++ src/core/dependencies/containers.py
@@ -9,6 +9,13 @@ from src.features.auth.application.use_cases.register_user import RegisterUserUs
 # from src.features.auth.application.use_cases.logout_user import LogoutUserUseCase
 # from src.features.auth.application.use_cases.verify_token import VerifyTokenUseCase
 
+from src.features.apiaries.infrastructure.repositories.apiary_repository_impl import ApiaryRepositoryImpl
+from src.features.apiaries.application.use_cases.create_apiary import CreateApiary
+from src.features.apiaries.application.use_cases.get_apiary_by_id import GetApiaryById
+from src.features.apiaries.application.use_cases.get_all_apiaries import GetAllApiaries
+from src.features.apiaries.application.use_cases.update_apiary import UpdateApiary
+from src.features.apiaries.application.use_cases.delete_apiary import DeleteApiary
+
 class MainContainer(containers.DeclarativeContainer):
     """Contenedor principal"""
     
@@ -50,6 +57,38 @@ class MainContainer(containers.DeclarativeContainer):
         RegisterUserUseCase,
         user_repository=user_repository,
         password_hasher=password_hasher
+    )
+    
+    # Repositorios de Apiary
+    apiary_repository = providers.Factory(
+        ApiaryRepositoryImpl,
+        db_session=db_session
+    )
+
+    # Casos de uso de Apiary
+    create_apiary_use_case = providers.Factory(
+        CreateApiary,
+        apiary_repository=apiary_repository
+    )
+
+    get_apiary_by_id_use_case = providers.Factory(
+        GetApiaryById,
+        apiary_repository=apiary_repository
+    )
+
+    get_all_apiaries_use_case = providers.Factory(
+        GetAllApiaries,
+        apiary_repository=apiary_repository
+    )
+
+    update_apiary_use_case = providers.Factory(
+        UpdateApiary,
+        apiary_repository=apiary_repository
+    )
+
+    delete_apiary_use_case = providers.Factory(
+        DeleteApiary,
+        apiary_repository=apiary_repository
     )
     
     # refresh_token_use_case = providers.Factory(
