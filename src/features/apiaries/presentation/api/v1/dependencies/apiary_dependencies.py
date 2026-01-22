59fc63bf6c75f0c2ee029d70006f94af259ce7c4
TheLostHeaven <dannyking07@outlook.es>
2026-01-22 16:10:52 -0500

    feat (HU-000005): fix imports to apiaries containers

--- src/features/apiaries/presentation/api/v1/dependencies/apiary_dependencies.py
+++ src/features/apiaries/presentation/api/v1/dependencies/apiary_dependencies.py
@@ -1,6 +1,5 @@
 from dependency_injector.wiring import Provide, inject
 from dependency_injector import containers, providers
-from fastapi import Depends
 from sqlalchemy.orm import Session
 from src.core.dependencies.containers import MainContainer
 from src.features.apiaries.application.use_cases.create_apiary import CreateApiary
@@ -13,36 +12,36 @@ from src.features.apiaries.infrastructure.repositories.apiary_repository_impl im
 
 @inject
 def get_apiary_repository(
-    db_session: Session = Depends(Provide[MainContainer.db_session]),
+    db_session: Session = (Provide[MainContainer.db_session]),
 ) -> ApiaryRepositoryImpl:
     return ApiaryRepositoryImpl(db_session=db_session)
 
 @inject
 def get_create_apiary_use_case(
-    apiary_repository: ApiaryRepositoryImpl = Depends(get_apiary_repository),
+    apiary_repository: ApiaryRepositoryImpl = (get_apiary_repository),
 ) -> CreateApiary:
     return CreateApiary(apiary_repository=apiary_repository)
 
 @inject
-def get_get_apiary_by_id_use_case(
-    apiary_repository: ApiaryRepositoryImpl = Depends(get_apiary_repository),
+def get_apiary_by_id_use_case(
+    apiary_repository: ApiaryRepositoryImpl = (get_apiary_repository),
 ) -> GetApiaryById:
     return GetApiaryById(apiary_repository=apiary_repository)
 
 @inject
-def get_get_all_apiaries_use_case(
-    apiary_repository: ApiaryRepositoryImpl = Depends(get_apiary_repository),
+def get_all_apiaries_use_case(
+    apiary_repository: ApiaryRepositoryImpl = (get_apiary_repository),
 ) -> GetAllApiaries:
     return GetAllApiaries(apiary_repository=apiary_repository)
 
 @inject
 def get_update_apiary_use_case(
-    apiary_repository: ApiaryRepositoryImpl = Depends(get_apiary_repository),
+    apiary_repository: ApiaryRepositoryImpl = (get_apiary_repository),
 ) -> UpdateApiary:
     return UpdateApiary(apiary_repository=apiary_repository)
 
 @inject
 def get_delete_apiary_use_case(
-    apiary_repository: ApiaryRepositoryImpl = Depends(get_apiary_repository),
+    apiary_repository: ApiaryRepositoryImpl = (get_apiary_repository),
 ) -> DeleteApiary:
     return DeleteApiary(apiary_repository=apiary_repository)
