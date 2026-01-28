2c1029ba6b41e6f491377992fef77fa51b96e272
TheLostHeaven <dannyking07@outlook.es>
2026-01-28 01:37:10 -0500

    feat (HU-000008): create user router export

--- src/features/user/presentation/api/v1/endpoints/__init__.py
+++ src/features/user/presentation/api/v1/endpoints/__init__.py
@@ -1,12 +1,4 @@
-from flask import Blueprint
 
-user_bp = Blueprint(
-    'user',
-    __name__,
-    url_prefix='/api/v1/user'
-)
-
-# Importar rutas
-from . import routes
+from .users import user_bp
 
 __all__ = ['user_bp']
